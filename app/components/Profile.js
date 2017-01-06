36c36c8f2d4a3daf2b044d1bdd9440440a12de33
Egemen Can Uze <egemencanuze@gmail.com>
2017-01-06 12:56:08 +0300

    Render my events on profile component

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
+import axios from 'axios';
 import { getUser } from '../actions';
 
 class Profile extends Component {
@@ -8,11 +9,30 @@ class Profile extends Component {
       this.props.getUser();
   }
 
+  handleLeave(business) {
+    axios.post('http://localhost:3000/api/user/leave', { business },
+      {
+        headers: { authorization: localStorage.getItem('token') }
+      }
+    ).then(() => {
+      this.props.getUser();
+    });
+  }
+
+  renderBusiness(business) {
+    return (
+      <div key={business}>
+        <span>{business}</span>
+        <button className="btn btn-danger" onClick={this.handleLeave.bind(this, business)}>I WON'T GO</button>
+      </div>
+    );
+  }
+
   render() {
     if(!this.props.user) {
       return <div>Loading...</div>
     }
-    const { email, lastSearch } = this.props.user;
+    const { email, lastSearch, events } = this.props.user;
 
     return (
       <div className="Profile">
@@ -23,6 +43,9 @@ class Profile extends Component {
         </div>
         <div>
           <h2>My Events</h2>
+          <div>
+            {events.map(business => this.renderBusiness(business)) }
+          </div>
         </div>
       </div>
     );
